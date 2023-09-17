a1afb68e64500548c1629f7a4753050b2fe96f1c
RyoshiAE <145283132+RyoshiAE@users.noreply.github.com>
2023-09-17 19:58:18 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import random 
+
+asd = ["Adobe After Effect ve Adobe Photoshop Kullanıyorum", "İskender En Sevdiğim Yemektir", "Editör Bir Arkadaşım İle 103 Bin Beğeni Alan Ortak Projemiz Var"]
+
+print(random.choice(asd))
